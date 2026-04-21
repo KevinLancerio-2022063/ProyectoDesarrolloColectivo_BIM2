709cfe917926eab9067bf65babc9b41270155341
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-04-21 11:17:31 -0600

    refactor: se ha cambiado la notacion de @RestController a @Controller en LIbro

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/LibroController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/LibroController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/libro")
 public class LibroController {
     private final LibroService libroService;
