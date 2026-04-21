b9056f2b5d181c2f413658ebea9b5abac117c4ab
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-04-21 10:50:38 -0600

    refactor: se ha cambiado la notacion de @RestController a @Controller en PerfilNutricionalController

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/PerfilNutricionalController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/PerfilNutricionalController.java
@@ -9,10 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/perfilNutricional")
+@Controller
+@RequestMapping("/perfilNutricional")
 public class PerfilNutricionalController {
     private final PerfilNutricionalService perfilNutricionalService;
 
