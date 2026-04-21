5427264cff52000fefead3073649cae1a37b161f
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-04-21 11:53:12 -0600

    feat: se agregaron las debidas importaciones

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/LibroController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/LibroController.java
@@ -4,12 +4,12 @@ package com.BIM1.ProyectoDesarrolloColectivo.Controllers;
 import com.BIM1.ProyectoDesarrolloColectivo.Entity.Libro;
 import com.BIM1.ProyectoDesarrolloColectivo.Service.LibroService;
 import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
 
 @Controller
 @RequestMapping("/libro")
--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/PerfilNutricionalController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/PerfilNutricionalController.java
@@ -4,10 +4,11 @@ package com.BIM1.ProyectoDesarrolloColectivo.Controllers;
 import com.BIM1.ProyectoDesarrolloColectivo.Entity.PerfilNutricional;
 import com.BIM1.ProyectoDesarrolloColectivo.Service.PerfilNutricionalService;
 import jakarta.validation.Valid;
-import org.hibernate.ObjectNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/perfilNutricional")
