c59b80fb1ddf442a9261aa3659296fc5f08ee045
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-04-21 09:21:34 -0600

    fix: se han modificado los metodos de LibroController para que tenga funcionalidad el crud

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/LibroController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/LibroController.java
@@ -4,48 +4,45 @@ package com.BIM1.ProyectoDesarrolloColectivo.Controllers;
 import com.BIM1.ProyectoDesarrolloColectivo.Entity.Libro;
 import com.BIM1.ProyectoDesarrolloColectivo.Service.LibroService;
 import jakarta.validation.Valid;
-import org.hibernate.ObjectNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/libro")
+@RequestMapping("/libro")
 public class LibroController {
     private final LibroService libroService;
 
     public LibroController(LibroService libroService) {
         this.libroService = libroService;
     }
+
     @GetMapping
-    public List<Libro> getAListLibro(){
-        return libroService.getAListLibro();
+    public String listarLibro(Model model){
+        model.addAttribute("libros", libroService.getAListLibro());
+        model.addAttribute("librosFormu", new Libro());
+        return "libro";
     }
 
-    @PostMapping
-    public ResponseEntity<Object> saveLibro(@Valid @RequestBody Libro libro){
-            Libro libro1 = libroService.saveLibro(libro);
-            return new ResponseEntity<>(libro1, HttpStatus.CREATED);
+    @GetMapping("/editarLibro/{id}")
+    public String editarLibro(@PathVariable Integer id, Model model){
+        model.addAttribute("libros", libroService.getAListLibro());
+        model.addAttribute("librosformu",libroService.getLibroById(id));
+        return "libro";
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> updateLibro(@PathVariable Integer id, @Valid @RequestBody Libro libro){
-            Libro libro1 = libroService.updateLibro(id, libro);
-            return new ResponseEntity<>(libro1, HttpStatus.OK);
+    @PostMapping("/guardarLibro")
+    public String guardarLibro(@Valid @ModelAttribute("librosFormu") Libro libro, BindingResult result, RedirectAttributes redirectAttributes, Model model){
+        if(result.hasErrors()){
+            model.addAttribute("libros", libroService.getAListLibro());
+            return "libro";
+        }
+        libroService.saveLibro(libro);
+        redirectAttributes.addFlashAttribute("exito", "el libro se ha guardado");
+        return "redirect:/libro";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteLibro(@PathVariable Integer id){
-            libroService.deleteLibro(id);
-            return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getLibroById(@PathVariable Integer id){
-            Libro libro = libroService.getLibroById(id);
-            return ResponseEntity.ok(libro);
-
-    }
 }
