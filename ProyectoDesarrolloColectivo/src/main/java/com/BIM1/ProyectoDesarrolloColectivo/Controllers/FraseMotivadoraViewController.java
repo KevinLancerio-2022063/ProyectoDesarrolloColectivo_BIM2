b4a831b6e1dc95f100aae6340e4cd161738baa5d
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-04-26 17:20:32 -0600

    feat: se agregaron metodos para agregarFrase en FraseMotivadoraViewController.java

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/FraseMotivadoraViewController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/FraseMotivadoraViewController.java
@@ -2,9 +2,13 @@ package com.BIM1.ProyectoDesarrolloColectivo.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.BIM1.ProyectoDesarrolloColectivo.Entity.FraseMotivadora;
 import com.BIM1.ProyectoDesarrolloColectivo.Service.FraseMotivadoraService;
 
 @Controller
@@ -16,6 +20,18 @@ public class FraseMotivadoraViewController {
     public String mostrarFraseMotivadora(org.springframework.ui.Model model) {
         model.addAttribute("frases", fraseMotivadoraService.getAllFraseMotivadora());
         return "FraseMotivadora";
+    }
+
+    @GetMapping("/agregarFrase")
+    public String agregarFraseMotivadora(Model model) {
+        model.addAttribute("frase", new FraseMotivadora());
+        return "agregarFrase";
+    }
+
+    @PostMapping("/guardarFraseCreada")
+    public String guardarFraseCreada(@ModelAttribute FraseMotivadora frase) {
+        fraseMotivadoraService.saveFraseMotivadora(frase);
+        return "redirect:/frasesMotivadoras";
     }
 
     @GetMapping("/eliminar-frase/{id}")
