f1f6d1b4ca16bf7c069a17cbb827db15793612d2
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-04-28 11:58:50 -0600

    feat: Se ha agregado la forma de ver en las tarjetas de éxito la información correspondiente

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/UsuarioViewController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/UsuarioViewController.java
@@ -2,13 +2,16 @@ package com.BIM1.ProyectoDesarrolloColectivo.Controllers;
 
 import com.BIM1.ProyectoDesarrolloColectivo.Entity.Usuario;
 import com.BIM1.ProyectoDesarrolloColectivo.Service.UsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/usuarios")
@@ -25,10 +28,28 @@ public class UsuarioViewController {
     }
 
     @PostMapping("/guardar")
-    public String guardar(Usuario usuario) {
+    public String guardar(@Valid Usuario usuario, BindingResult result, Model model, RedirectAttributes redirect) {
+
+        if (result.hasErrors()) {
+            model.addAttribute("usuarios", service.getAllUsuarios());
+            model.addAttribute("errores", result.getFieldErrors().stream().map(e -> e.getDefaultMessage()).toList());
+
+            return "usuarios";
+        }
+
+        boolean nuevaCuenta = (usuario.getId_usuario() == null);
+
         service.saveUsuario(usuario);
+
+        if (nuevaCuenta) {
+            redirect.addFlashAttribute("success", "Tu cuenta se ha agregado correctamente " + usuario.getNombre_completo());
+        } else {
+            redirect.addFlashAttribute("success", usuario.getNombre_completo() + " se ha actualizado correctamente");
+        }
+
         return "redirect:/usuarios";
     }
+
 
     @GetMapping("/editar/{id}")
     public String editar(@PathVariable Integer id, Model model) {
@@ -38,8 +59,12 @@ public class UsuarioViewController {
     }
 
     @GetMapping("/eliminar/{id}")
-    public String eliminar(@PathVariable Integer id) {
+    public String eliminar(@PathVariable Integer id, RedirectAttributes redirect) {
+
+        Usuario usuario = service.getUsuariosById(id);
+
         service.deleteUsuario(id);
+        redirect.addFlashAttribute("success", usuario.getNombre_completo() +" se ha eliminado correctamente");
         return "redirect:/usuarios";
     }
 
