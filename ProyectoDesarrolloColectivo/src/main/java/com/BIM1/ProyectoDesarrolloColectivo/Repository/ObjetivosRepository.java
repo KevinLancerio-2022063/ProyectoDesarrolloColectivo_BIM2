7318ff0558ba6ab14a8fa350992c1af246def3fc
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-04-27 20:05:22 -0600

    refactor: se cambio el tipo de dato de fechaObjetivo, de Date a LocalDate

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/ObjetivosRepository.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/ObjetivosRepository.java
@@ -5,13 +5,14 @@ import com.BIM1.ProyectoDesarrolloColectivo.Entity.Objetivos;
 import com.BIM1.ProyectoDesarrolloColectivo.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Date;
+import java.time.LocalDate;
+
 
 public interface ObjetivosRepository extends JpaRepository<Objetivos,Integer> {
     boolean existsByDescripcionObjetivoAndEstadoObjetivoAndFechaObjetivoAndUsuarioAndFraseMotivadora(
             String descripcionObjetivo,
             String estadoObjetivo,
-            Date fechaObjetivo,
+            LocalDate fechaObjetivo,
             Usuario usuario,
             FraseMotivadora fraseMotivadora
     );
