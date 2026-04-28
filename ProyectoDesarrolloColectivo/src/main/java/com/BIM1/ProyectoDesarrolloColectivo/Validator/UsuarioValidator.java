345af36a0c06fb3d7cfabb2f8d26df3a2fb78dc0
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-04-28 11:55:23 -0600

    fix: Se ha corregido la validación de no poder editar un usuario por su correo en UsuarioValidator

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Validator/UsuarioValidator.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Validator/UsuarioValidator.java
@@ -29,7 +29,16 @@ public class UsuarioValidator {
         }
 
         for (Usuario correoUsua : usuarios) {
-            if (correoUsu.equals(correoUsua.getCorreoUsuario())){
+
+            if (correoUsu.equals(correoUsua.getCorreoUsuario())) {
+
+                // Si el correo es del mismo usuario pasará de este
+                if (usuario.getId_usuario() != null &&
+                        usuario.getId_usuario().equals(correoUsua.getId_usuario())) {
+                    // El continue es para saltarse la iteración actual
+                    continue;
+                }
+
                 throw new IllegalArgumentException("El correo del usuario ya existe");
             }
         }
