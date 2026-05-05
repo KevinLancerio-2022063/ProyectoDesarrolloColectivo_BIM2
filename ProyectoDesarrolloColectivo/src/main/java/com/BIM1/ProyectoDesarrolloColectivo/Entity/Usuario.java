e80ceab13655a898d71f13599610444d00d67591
fernandoherrarte-2021072 <fherrarte-2021072@kinal.edu.gt>
2026-05-05 11:52:30 -0600

    feat: Se mapeo la columna rol en el Entity de Usuario

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Usuario.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/Usuario.java
@@ -27,6 +27,9 @@ public class Usuario {
     @Column(name = "contraseña")
     private String contraseña;
 
+    @Column(name = "rol")
+    private String rol;
+
     public Integer getId_usuario() {
         return id_usuario;
     }
@@ -58,4 +61,9 @@ public class Usuario {
     public void setContraseña(String contraseña) {
         this.contraseña = contraseña;
     }
+
+    public String getRol() { return rol;}
+
+    public void setRol(String rol) { this.rol = rol; }
+
 }
