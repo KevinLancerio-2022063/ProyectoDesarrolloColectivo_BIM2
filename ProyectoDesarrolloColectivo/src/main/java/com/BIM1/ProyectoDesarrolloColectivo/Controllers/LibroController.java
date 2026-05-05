ab1d8834470b46cdcfb9b95cab0a8637a91a6c14
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-05-04 19:25:25 -0600

    feat: se agregaron las debidas importaciones de List y Collector en el controller de libro

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/LibroController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/LibroController.java
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Controller
