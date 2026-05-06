b57bd4990fcf02baac932e8b133e7b1f774b72ab
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-05-06 10:46:36 -0600

    feat: Se ha agregado el script para modificar el cursor del mouse en registroSueno.js

--- ProyectoDesarrolloColectivo/src/main/resources/static/js/registroSueno.js
+++ ProyectoDesarrolloColectivo/src/main/resources/static/js/registroSueno.js
@@ -19,24 +19,52 @@ function filtrarTabla() {
 
 let deleteUrl = "";
 
-// abrir modal
-function openModal(url) {
-    deleteUrl = url;
-    document.getElementById("confirmModal").classList.remove("hidden");
-}
-
-// cerrar modal
-function closeModal() {
-    document.getElementById("confirmModal").classList.add("hidden");
-}
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const btn = document.getElementById("btnDelete");
-
-    if (btn) {
-        btn.addEventListener("click", () => {
-            window.location.href = deleteUrl;
-        });
+    // abrir modal
+    function openModal(url) {
+        deleteUrl = url;
+        document.getElementById("confirmModal").classList.remove("hidden");
     }
-});
+
+    // cerrar modal
+    function closeModal() {
+        document.getElementById("confirmModal").classList.add("hidden");
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const btn = document.getElementById("btnDelete");
+
+        if (btn) {
+            btn.addEventListener("click", () => {
+                window.location.href = deleteUrl;
+            });
+        }
+    });
+
+// Script del cursor del mouse
+const dot = document.querySelector(".cursor-dot");
+    const ring = document.querySelector(".cursor-ring");
+
+    window.addEventListener("mousemove", function(e) {
+        dot.style.left = e.clientX + "px";
+        dot.style.top = e.clientY + "px";
+
+        ring.animate({
+            left: e.clientX + "px",
+            top: e.clientY + "px"
+        }, {
+            duration: 420,
+            fill: "forwards"
+        });
+
+        const trail = document.createElement("span");
+        trail.className = "cursor-trail";
+        trail.style.left = e.clientX + "px";
+        trail.style.top = e.clientY + "px";
+        document.body.appendChild(trail);
+
+        setTimeout(() => trail.remove(), 600);
+    });
+    document.querySelectorAll('a, button, .btn-icon, .usuario-card').forEach(el => {
+        el.addEventListener('mouseenter', () => { ring.style.width='48px'; ring.style.height='48px'; ring.style.borderColor='var(--cafe-claro)'; });
+        el.addEventListener('mouseleave', () => { ring.style.width='32px'; ring.style.height='32px'; ring.style.borderColor='var(--verde-menta)'; });
+    });
