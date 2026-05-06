e7e558ab9b1504f217b9dc412a1fadb17c6adba8
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-05-06 11:32:52 -0600

    fix: se cambio el tipo de dato de peso_kg en la entity de PerfilNutricional

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/PerfilNutricional.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Entity/PerfilNutricional.java
@@ -18,7 +18,7 @@ public class PerfilNutricional {
     @Positive(message = "el peso debe de ser mayor a 0")
     @Max(value = 300, message = "el peso no puede ser mayor a 300 kg")
     @Min(value = 45, message = "el peso no puede ser menor a 45 kg")
-    private int peso_kg;
+    private Double peso_kg;
 
     @Column(name = "altura")
     @NotNull(message = "la altura no puede estar vacia")
@@ -59,59 +59,59 @@ public class PerfilNutricional {
         this.id_perfil_nutricional = id_perfil_nutricional;
     }
 
-    public int getPeso_kg() {
+    public @NotNull(message = "el peso no puede estar vacio o nulo") @Positive(message = "el peso debe de ser mayor a 0") @Max(value = 300, message = "el peso no puede ser mayor a 300 kg") @Min(value = 45, message = "el peso no puede ser menor a 45 kg") Double getPeso_kg() {
         return peso_kg;
     }
 
-    public void setPeso_kg(int peso_kg) {
+    public void setPeso_kg(@NotNull(message = "el peso no puede estar vacio o nulo") @Positive(message = "el peso debe de ser mayor a 0") @Max(value = 300, message = "el peso no puede ser mayor a 300 kg") @Min(value = 45, message = "el peso no puede ser menor a 45 kg") Double peso_kg) {
         this.peso_kg = peso_kg;
     }
 
-    public Double getAltura() {
+    public @NotNull(message = "la altura no puede estar vacia") @DecimalMin(value = "0.50", message = "la altura minima valida es de 0.5 metros") @DecimalMax(value = "2.50", message = "la altura maxima valida es de 2.5 metros") Double getAltura() {
         return altura;
     }
 
-    public void setAltura(Double altura) {
+    public void setAltura(@NotNull(message = "la altura no puede estar vacia") @DecimalMin(value = "0.50", message = "la altura minima valida es de 0.5 metros") @DecimalMax(value = "2.50", message = "la altura maxima valida es de 2.5 metros") Double altura) {
         this.altura = altura;
     }
 
-    public int getEdad() {
+    public @NotNull(message = "la edad no puede ser vacia ni nula") @Positive(message = "la edad no puede ser menor o igual a 0 ") @Min(value = 12, message = "la edad minima debe ser de 12 años") @Max(value = 120, message = "la edad no puede superar los 120 años") int getEdad() {
         return edad;
     }
 
-    public void setEdad(int edad) {
+    public void setEdad(@NotNull(message = "la edad no puede ser vacia ni nula") @Positive(message = "la edad no puede ser menor o igual a 0 ") @Min(value = 12, message = "la edad minima debe ser de 12 años") @Max(value = 120, message = "la edad no puede superar los 120 años") int edad) {
         this.edad = edad;
     }
 
-    public String getGenero() {
+    public @NotBlank(message = "el genero no puede ser vacio") String getGenero() {
         return genero;
     }
 
-    public void setGenero(String genero) {
+    public void setGenero(@NotBlank(message = "el genero no puede ser vacio") String genero) {
         this.genero = genero;
     }
 
-    public String getNivel_actividad() {
+    public @NotBlank(message = "el nivel de actividad no puede ser vacio") String getNivel_actividad() {
         return nivel_actividad;
     }
 
-    public void setNivel_actividad(String nivel_actividad) {
+    public void setNivel_actividad(@NotBlank(message = "el nivel de actividad no puede ser vacio") String nivel_actividad) {
         this.nivel_actividad = nivel_actividad;
     }
 
-    public String getObjetivo() {
+    public @NotBlank(message = "el objetivo no puede estar vacio") @Size(max = 75, message = "el mensaje no puede tener mas de 75 caracteres") String getObjetivo() {
         return objetivo;
     }
 
-    public void setObjetivo(String objetivo) {
+    public void setObjetivo(@NotBlank(message = "el objetivo no puede estar vacio") @Size(max = 75, message = "el mensaje no puede tener mas de 75 caracteres") String objetivo) {
         this.objetivo = objetivo;
     }
 
-    public Integer getFk_id_usuario() {
+    public @NotNull(message = "el id del usuario no puede ser vacio ni nulo") @Positive(message = "el id de la llave foranea no pude ser menor o igual 0") Integer getFk_id_usuario() {
         return fk_id_usuario;
     }
 
-    public void setFk_id_usuario(Integer fk_id_usuario) {
+    public void setFk_id_usuario(@NotNull(message = "el id del usuario no puede ser vacio ni nulo") @Positive(message = "el id de la llave foranea no pude ser menor o igual 0") Integer fk_id_usuario) {
         this.fk_id_usuario = fk_id_usuario;
     }
 }
