f67247d90ba8100786e1c84adc1dba766ef478c4
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-05-06 09:22:35 -0600

    feat: se agrego funcionalidad a los  elementos del cursor personalizado

--- ProyectoDesarrolloColectivo/src/main/resources/static/js/Base.js
+++ ProyectoDesarrolloColectivo/src/main/resources/static/js/Base.js
@@ -8,3 +8,27 @@ function cerrarModal() {
     document.getElementById("modal-confirmar").style.display = "none";
 }
 
+
+const dot = document.querySelector(".cursor-dot");
+    const ring = document.querySelector(".cursor-ring");
+
+    window.addEventListener("mousemove", function(e) {
+        dot.style.left = e.clientX + "px";
+        dot.style.top = e.clientY + "px";
+
+        ring.animate({
+            left: e.clientX + "px",
+            top: e.clientY + "px"
+        }, {
+            duration: 420,
+            fill: "forwards"
+        });
+
+        const trail = document.createElement("span");
+        trail.className = "cursor-trail";
+        trail.style.left = e.clientX + "px";
+        trail.style.top = e.clientY + "px";
+        document.body.appendChild(trail);
+
+        setTimeout(() => trail.remove(), 600);
+    });
