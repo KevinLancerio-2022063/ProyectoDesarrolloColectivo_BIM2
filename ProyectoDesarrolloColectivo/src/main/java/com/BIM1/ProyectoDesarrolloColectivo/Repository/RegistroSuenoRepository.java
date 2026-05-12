c6d92c5f5f55307fcf7b8f9432e4cfb33cc2789b
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-05-12 03:57:09 -0600

    feat: Se ha agregado el método para buscar por el FkidUsuario en RegistroSuenoRepository

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/RegistroSuenoRepository.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/RegistroSuenoRepository.java
@@ -4,7 +4,10 @@ import com.BIM1.ProyectoDesarrolloColectivo.Entity.RegistroSueno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RegistroSuenoRepository extends JpaRepository<RegistroSueno, Integer> {
 
+    List<RegistroSueno> findByFkIdUsuario(Integer fkIdUsuario);
 }
