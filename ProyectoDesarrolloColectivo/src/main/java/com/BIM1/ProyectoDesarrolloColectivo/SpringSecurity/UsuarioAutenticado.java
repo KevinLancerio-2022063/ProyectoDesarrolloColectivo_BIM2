cb8126e9941df0352285f50ece7caaca2e5c35ec
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-05-12 03:36:34 -0600

    refactor: Se ha colocado el ROLE_ y eliminado código innecesario del token

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/SpringSecurity/UsuarioAutenticado.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/SpringSecurity/UsuarioAutenticado.java
@@ -2,9 +2,7 @@ package com.BIM1.ProyectoDesarrolloColectivo.SpringSecurity;
 
 import com.BIM1.ProyectoDesarrolloColectivo.Entity.Usuario;
 import com.BIM1.ProyectoDesarrolloColectivo.Repository.UsuarioRepository;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -17,25 +15,13 @@ public class UsuarioAutenticado {
         this.usuarioRepository = usuarioRepository;
     }
 
-    // Obtiene el usuarioId desde los detalles del token
     public Integer getId() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-        if (auth instanceof UsernamePasswordAuthenticationToken token) {
-            Object details = token.getDetails();
-            if (details instanceof Integer) {
-                return (Integer) details;
-            }
-        }
-
         return getUsuarioDeBD().getId_usuario();
     }
 
-    // Obtiene el rol desde las authorities del token
     public String getRol() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-        return auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).findFirst().orElse("USER").replace("ROLE", "");
+        return auth.getAuthorities().stream().findFirst().map(a -> a.getAuthority().replace("ROLE_", "")).orElse("USER");
     }
 
     public String getNombre() {
@@ -43,7 +29,6 @@ public class UsuarioAutenticado {
         return auth.getName();
     }
 
-    // Solo se llama si no hay token (fallback)
     private Usuario getUsuarioDeBD() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return usuarioRepository.findByCorreoUsuario(auth.getName());
