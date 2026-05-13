6bac1fb39ee595f5896d8102658ab7a8f8147ec1
Cristopher <clopez-2021325@kinal.edu.gt>
2026-05-12 22:09:04 -0600

    feat: El service no funcionaba y agregue mejoras

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/EntradaDiarioService.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/EntradaDiarioService.java
@@ -5,12 +5,9 @@ import com.BIM1.ProyectoDesarrolloColectivo.Entity.EntradaDiario;
 import java.util.List;
 
 public interface EntradaDiarioService {
-    List<EntradaDiario> getEntradaByUsuario(Integer idUsuario);
     List<com.BIM1.ProyectoDesarrolloColectivo.Entity.EntradaDiario> getAListEntradaDiario();
     EntradaDiario getEntradaDiarioById(Integer id);
     EntradaDiario saveEntradaDiario(EntradaDiario entradaDiario) throws RuntimeException;
     EntradaDiario updateEntradaDiario(Integer id, EntradaDiario entradaDiario);
     void deleteEntradaDiario(Integer id);
-
 }
-
