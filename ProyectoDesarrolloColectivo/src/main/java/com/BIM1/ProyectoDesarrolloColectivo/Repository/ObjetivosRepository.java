c2bc970995c60bdff822bcc2f519e8401b009641
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-05-12 23:11:38 -0600

    Update agregarObjetivo.html
    refactor:se agrego metodo para mostrar los datos del usuario en ObjetivosRepository.java

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/ObjetivosRepository.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/ObjetivosRepository.java
@@ -4,8 +4,11 @@ import com.BIM1.ProyectoDesarrolloColectivo.Entity.FraseMotivadora;
 import com.BIM1.ProyectoDesarrolloColectivo.Entity.Objetivos;
 import com.BIM1.ProyectoDesarrolloColectivo.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.util.List;
 
 
 public interface ObjetivosRepository extends JpaRepository<Objetivos,Integer> {
@@ -17,4 +20,7 @@ public interface ObjetivosRepository extends JpaRepository<Objetivos,Integer> {
             Usuario usuario,
             FraseMotivadora fraseMotivadora
     );
+
+    @Query("SELECT o FROM Objetivos o WHERE o.usuario.id_usuario = :id_usuario")
+    List<Objetivos> findByUsuarioId(@Param("id_usuario") Integer id_usuario);
 }
