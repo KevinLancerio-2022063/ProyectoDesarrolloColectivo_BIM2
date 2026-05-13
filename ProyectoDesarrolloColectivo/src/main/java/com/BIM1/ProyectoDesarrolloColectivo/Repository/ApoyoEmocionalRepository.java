d9c313c35f3223fec9386178407bc2f75fe6f5af
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-05-12 23:10:50 -0600

    Update agregarObjetivo.html
    refactor:se agrego metodo para mostrar los datos del usuario en ApoyoEmocionalRepository

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/ApoyoEmocionalRepository.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Repository/ApoyoEmocionalRepository.java
@@ -3,10 +3,14 @@ package com.BIM1.ProyectoDesarrolloColectivo.Repository;
 import com.BIM1.ProyectoDesarrolloColectivo.Entity.ApoyoEmocional;
 import com.BIM1.ProyectoDesarrolloColectivo.Entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ApoyoEmocionalRepository extends JpaRepository<ApoyoEmocional,Integer> {
+public interface ApoyoEmocionalRepository extends JpaRepository<ApoyoEmocional, Integer> {
 
     boolean existsByTituloAndCategoriaAndContenidoAndNivelAnimoAndUsuario(
             String titulo,
@@ -15,4 +19,7 @@ public interface ApoyoEmocionalRepository extends JpaRepository<ApoyoEmocional,I
             String nivelAnimo,
             Usuario usuario
     );
+
+    @Query("SELECT a FROM ApoyoEmocional a WHERE a.usuario.id_usuario = :idUsuario")
+    List<ApoyoEmocional> findByIdUsuario(@Param("idUsuario") Integer idUsuario);
 }
