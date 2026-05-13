b401a365fc4927fb70688a151875f7e71cfd2029
kevinjuarez-2025054 <kjuarez-2025054@kinal.edu.gt>
2026-05-12 23:11:09 -0600

    Update agregarObjetivo.html
    refactor:se agrego metodo para mostrar los datos del usuario en ApoyoEmocionalService.java

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/ApoyoEmocionalService.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Service/ApoyoEmocionalService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface ApoyoEmocionalService {
     List<ApoyoEmocional> getAllApoyoEmovional();
     ApoyoEmocional getById(Integer id);
+    List<ApoyoEmocional> getByIdUsuario(Integer id);
     ApoyoEmocional saveApoyoEmocional(ApoyoEmocional apoyoEmocional) throws RuntimeException;
     ApoyoEmocional updateApoyoEmocional(Integer id, ApoyoEmocional apoyoEmocional);
     void deleteApoyoEmocional(Integer id);
