783894d290f3f5d04bbc9144bd17efce73b5714d
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-05-19 04:37:50 -0600

    removed: Se han eliminado rutas innecesarias en el HomeController

--- ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/HomeController.java
+++ ProyectoDesarrolloColectivo/src/main/java/com/BIM1/ProyectoDesarrolloColectivo/Controllers/HomeController.java
@@ -2,7 +2,6 @@ package com.BIM1.ProyectoDesarrolloColectivo.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -10,30 +9,6 @@ public class HomeController {
     @GetMapping("/")
     public String index() {
         return "index";
-    }
-    @GetMapping("/logn")
-    public String login() {
-        return "login";
-    }
-    @PostMapping("/loin")
-    public String procesarLogn() {
-        return "redirect:/register";
-    }
-    @GetMapping("/regiser")
-    public String regiter() {
-        return "register";
-    }
-    @GetMapping("/rutinas")
-    public String rutinas() {
-        return "entradaDiario";
-    }
-    @GetMapping("/bienestar")
-    public String bienestar() {
-        return "RegistroMeditacion";
-    }
-    @GetMapping("/nutricion")
-    public String nutricion() {
-        return "entradaDiario";
     }
 
     @GetMapping("/home")
